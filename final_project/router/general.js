ead22c927d4588a44ecc8b9608e8c09ec8bf9df8
kamibly <kamibly@gmail.com>
2024-05-13 14:00:12 -0400

    New Update files - final assignment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,36 +7,176 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.query.username;
+  const password = req.query.password;
+    /*
+    const doesExist = (username)=>{
+        let userswithsamename = users.filter((user)=>{
+          return user["username"] === username
+        });
+        if(userswithsamename.length > 0){
+            res.send(userswithsamename.length);
+          return true;
+        } else {
+          return false;
+        }
+    }
+    */
+    if (username && password) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "Corret the username and/or password!"});    
+    }
+  
+    //return res.status(300).json({message: "Yet to be implemented"});
+
 });
+
+// TASK 10 - Get the book list available in the shop using Promises
+public_users.get('/books',function (req, res) {
+
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+
+  });
+
+// TASK 11 - Get book details based on ISBN using Promises
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const get_books_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    // console.log(isbn);
+        if (req.params.isbn <= 10) {
+        resolve(res.send(books[isbn]));
+    }
+        else {
+            reject(res.send('ISBN not found'));
+        }
+    });
+    get_books_isbn.
+        then(function(){
+            console.log("Promise for Task 11 is resolved");
+   }).
+        catch(function () { 
+                console.log('ISBN not found');
+  });
+
+});
+
+// TASK 12 - Get book details based on author
+public_users.get('/books/author/:author',function (req, res) {
+
+    const get_books_author = new Promise((resolve, reject) => {
+
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
+      }
+
+
+    });
+    reject(res.send("The mentioned author does not exist "))
+        
+    });
+
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
+  });
+
+  });
+
+  // TASK 13 - Get book details based on title
+public_users.get('/books/title/:title',function (req, res) {
+
+    const get_books_title = new Promise((resolve, reject) => {
+
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+                            "author":books[isbn]["author"],
+                            "reviews":books[isbn]["reviews"]});
+      resolve(res.send(JSON.stringify({booksbytitle}, null, 4)));
+      }
+
+
+    });
+    reject(res.send("The mentioned title does not exist"))
+        
+    });
+
+    get_books_title.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
+  });
+
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books))
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+    const isbn = req.params.isbn
+    res.send(JSON.stringify(books[isbn]))
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbyauthor}, null, 4));
+      //const author = req.params.author
+      //let filtered_author = books.filter((book)=>book.author ===author);
+      //res.send(filtered_author);
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
+    let book = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        book.push({"isbn":isbn,
+                            "author":books[isbn]["author"],
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"
+                            
+                            ]});
+      }
+    });
+    res.send(JSON.stringify({book}, null, 4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+    const isbn = req.params.isbn
+    res.send(JSON.stringify(books[isbn]["reviews"]))
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
